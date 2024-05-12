d5712fe8448b80389b049625afd3514922c0d7cf
memento <claude.rc@gmail.com>
2024-05-12 11:06:56 -0500

    (dbg) getting warmer?

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,8 +10,11 @@ set "SBCL_HOME=%INSTALL_ROOT%"
 set "PATH=%SBCL_HOME%;%PATH%"
 
 cd %SRC_DIR%\sbcl-source
+  dir %CONDA_PREFIX%\bin\xargs.exe
+  dir %CONDA_PREFIX%\bin\xargs
+  dir %PREFIX%\bin\xargs.exe
+  dir %PREFIX%\bin\xargs
   copy %CONDA_PREFIX%\bin\xargs.exe xargs
-  dir .\xargs
 
   bash make.sh --fancy
 
