036c8a063cc79619b8011d8a43aedcc485c457c0
memento <claude.rc@gmail.com>
2024-05-13 16:01:08 -0500

    (dbg) and another. test the full install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,7 +19,7 @@ cd %SRC_DIR%\sbcl-source
   copy %SRC_DIR%\sbcl-source\COPYING %SRC_DIR%\COPYING
   copy %SRC_DIR%\sbcl-source\CREDITS %SRC_DIR%\CREDIT
 
-  set INSTALL_ROOT=%PREFIX%
+  set "INSTALL_ROOT=%PREFIX%"
   set "SBCL_HOME=%INSTALL_ROOT%"
   bash install.sh
 cd %SRC_DIR%
