74910684aa954af066e711e7e0047e89f88f7cbf
memento <claude.rc@gmail.com>
2024-05-14 09:48:08 -0500

    (ref) reorg

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -30,8 +30,8 @@ copy %SRC_DIR%\sbcl-source\CREDITS %SRC_DIR%\CREDITS
 if not exist "%PREFIX%\etc\conda\activate.d\" mkdir "%PREFIX%\etc\conda\activate.d\"
 if not exist "%PREFIX%\etc\conda\deactivate.d\" mkdir "%PREFIX%\etc\conda\deactivate.d\"
 
-echo @echo off > "%PREFIX%\etc\conda\activate.d\activate_sbcl.bat"
-echo set SBCL_HOME=%CONDA_PREFIX%\lib\sbcl >> "%PREFIX%\etc\conda\activate.d\activate_sbcl.bat"
-
-echo @echo off > "%PREFIX%\etc\conda\deactivate.d\deactivate_sbcl.bat"
-echo set SBCL_HOME= >> "%PREFIX%\etc\conda\deactivate.d\deactivate_sbcl.bat"
+copy "%RECIPE_DIR%\scripts\activate.bat" "%PREFIX%\etc\conda\activate.d\sbcl-activate.bat" > nul
+if errorlevel 1 exit 1
+￼copy "%RECIPE_DIR%\scripts\activate.bat" "%PREFIX%\etc\conda\activate.d\sbcl-deactivate.bat" > nul
+if errorlevel 1 exit 1
+￼
---
+++ recipe/scripts/activate.bat
@@ -0,0 +1,5 @@
+@echo off
+@if defined SBCL_HOME (
+     @set "_SBCL_HOME_BACKUP=%SBCL_HOME%"
+)
+@set "SBCL_HOME=%CONDA_PREFIX%\lib\sbcl"
