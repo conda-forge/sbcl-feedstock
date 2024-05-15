1d4c10e5707be7b5ac9e2c559c6586e98f86547d
memento <claude.rc@gmail.com>
2024-05-15 16:12:40 -0500

    (fix) full path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -23,13 +23,13 @@ cd %SRC_DIR%\sbcl-source
   cd tests
     set "file_ext=*.sh *.lisp"
     set "search_string=\"/bin/sh\""
-    set "replacement_string=\"bash\""
+    set "replacement_string=\"C:\\Windows\\System32\\bash.exe\""
 
     where bash
 
     :: Use for, findstr, and powershell to replace the string in the list of files
     for %%G in (%file_ext%) do (
-      for /R "." %%F in (%%G) do (
+      for %%F in (%%G) do (
           findstr /M /C:"%search_string%" "%%F" >nul && (
               powershell -Command "(Get-Content '%%F') -replace '%search_string%', '%replacement_string%' | Set-Content '%%F'"
           )
