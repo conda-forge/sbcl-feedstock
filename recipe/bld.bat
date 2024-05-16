d0e5038f5c8f063c148201376f5922d88a2a83bb
memento <claude.rc@gmail.com>
2024-05-16 18:06:04 -0500

    (dbg) try to understand how to strength testing

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,9 +9,9 @@ copy "%INSTALL_ROOT%\sbcl.exe" "%INSTALL_ROOT%\sbcl"
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 :: Verify that the bootstrap runs the tests
-:: copy "%INSTALL_ROOT%\sbcl" "%SRC_DIR%\src\runtime\sbcl" > nul
-:: cd tests && bash run-tests.sh
-:: if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+copy "%INSTALL_ROOT%\sbcl" "%SRC_DIR%\src\runtime\sbcl" > nul
+cd tests && bash run-tests.sh
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 set "SBCL_HOME=%INSTALL_ROOT%"
 set "PATH=%SBCL_HOME%;%PATH%"
