3ee336f99dbd00deccd3ae9243b0c78dead32f3b
memento <claude.rc@gmail.com>
2024-05-25 15:24:02 -0500

    incomplete

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-# --- Functions ---
+set -ex
 
+XC_HOST="--xc-host='sbcl --disable-debugger --no-userinit --no-sysinit'"
 if [[ "${target_platform}" == "linux-ppc64le" ]]; then
-  XC_HOST=" --xc-host=x86_64-linux"
+  SBCL_ARGS="${XC_HOST}  --arch=ppc64le"
 elif [[ "${target_platform}" == "linux-aarch64" ]]; then
-  echo "Building for aarch64"
-  XC_HOST=" --xc-host=x86_64-linux"
+  SBCL_ARGS="${XC_HOST}  --arch=arm64"
 elif [[ "${target_platform}" == "osx-arm64" ]]; then
-  XC_HOST=" --xc-host=arm64-darwin"
+  SBCL_ARGS="${XC_HOST}  --arch=arm64"
 else
-  XC_HOST=""
+  SBCL_ARGS=""
 fi
 
 # if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
@@ -21,100 +21,53 @@ fi
 # fi
 
 function build_install_stage() {
-  # Builds SBCL under a custom stage_dir by copying the source files from src_dir (keeps original src)
-  # Installs SBCL under install_dir
   local src_dir=$1
   local stage_dir=$2
   local install_dir=$3
-
-  # Remember the current directory
+  local final=${4:-false}
   local current_dir
+
   current_dir=$(pwd)
 
-  # Prepare the stage/build directory
   mkdir -p "${stage_dir}"
   cp -r "${src_dir}"/* "${stage_dir}"
 
-  # Configure SBCL build arguments, like host architecture, enable fancy features
-  if [[ "${target_platform}" == "osx-arm64" ]]; then
-    SBCL_ARGS=(--fancy --arch=arm64)
-  else
-    SBCL_ARGS=(--fancy)
-  fi
-
-  # Build and install SBCL
   cd "${stage_dir}"
-    bash make.sh "${SBCL_ARGS[@]}" > _sbcl_build.log 2>&1
+    if [[ "${final}" == "true" ]]; then
+      bash make.sh --fancy ${SBCL_ARGS}
+    else
+      bash make.sh > _sbcl_build_log.txt 2>&1
+    fi
 
     INSTALL_ROOT=${install_dir}
     SBCL_HOME=${INSTALL_ROOT}/lib/sbcl
     export INSTALL_ROOT SBCL_HOME PATH=${INSTALL_ROOT}/bin:${PATH}
     bash install.sh
-
-    # Build shared library on linux systems
-    bash make-shared-library.sh > _sbcl_lib_build.log 2>&1
-    install -m 644 src/runtime/libsbcl.so "${install_dir}/lib/libsbcl.so"
-
-    if [[ $(uname) == Darwin ]]; then
-      ln -s "${install_dir}/lib/libsbcl.so" "${install_dir}/lib/libsbcl.dylib"
-    fi
-
-    if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "0" ]]; then
-      strip "${install_dir}"/bin/sbcl
-    fi
   cd "${current_dir}"
 }
 
-# --- Main ---
-
-set -ex
-
-# Select the conda architectures that build from source
 if [[ "${target_platform}" == "osx-64" ]] || \
-   [[ "${target_platform}" == "linux-64" ]]
+   [[ "${target_platform}" == "linux-aarch64" ]]
 then
-  # When not cross-compiling, the existing SBCL needs to be installed in the build environment
-  if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "0" ]]; then
-    mamba install -y sbcl
-    export SBCL_HOME=${BUILD_PREFIX}/lib/sbcl
-    export CROSSCOMPILING_EMULATOR=""
-  fi
-  # When cross-compiling, the build SBCL is installed in the build environment as a dependency
-
-  build_install_stage "${SRC_DIR}/sbcl-source" "${SRC_DIR}/_conda-build" "${PREFIX}"
-
-  # Copy the license and credits for conda-recipe packaging
-  cp "${SRC_DIR}"/sbcl-source/COPYING "${SRC_DIR}"
-  cp "${SRC_DIR}"/sbcl-source/CREDITS "${SRC_DIR}"
-
-# PPC64LE: no previous conda version: Need to bootstrap. Once a version is released
-# this special case will be merged to the above
-elif [[ "${target_platform}" == "linux-ppc64le" ]] || [[ "${target_platform}" == "linux-aarch64" ]]; then
-  # Install the bootstrap binary in a temporary location
-  export INSTALL_ROOT=${SRC_DIR}/_conda_bootstrap-install
-  export SBCL_HOME=${INSTALL_ROOT}/lib/sbcl
-  sh install.sh > _sbcl_bootstrap-install.log 2>&1
-
-  export PATH=${INSTALL_ROOT}/bin:${PATH}
-
-  # Build SBCL from source
-  build_install_stage "${SRC_DIR}/sbcl-source" "${SRC_DIR}/_conda-build" "${PREFIX}"
-
-  # Copy the license and credits for conda-recipe packaging
-  cp "${SRC_DIR}"/sbcl-source/COPYING "${SRC_DIR}"
-  cp "${SRC_DIR}"/sbcl-source/CREDITS "${SRC_DIR}"
-
-# All other architectures install the pre-built SBCL (downloaded in SRC_DIR
+  mamba install -y sbcl
+  build_install_stage "${SRC_DIR}/sbcl-source" "${SRC_DIR}/_conda_stage1-build" "${SRC_DIR}/_conda_stage1-install" "true"
+  cp -r "${INSTALL_ROOT}"/* "${PREFIX}" > /dev/null 2>&1
 else
   export INSTALL_ROOT=$PREFIX
-  export SBCL_HOME=${INSTALL_ROOT}/lib/sbcl
   sh install.sh
 fi
 
-# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
-# This will allow them to be run on environment activation.
-for CHANGE in "activate" "deactivate"
-do
-    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}-${CHANGE}.sh"
-done
+cp "${SRC_DIR}"/sbcl-source/COPYING "${SRC_DIR}"
+cp "${SRC_DIR}"/sbcl-source/CREDITS "${SRC_DIR}"
+
+# Install SBCL in conda-forge environment
+ACTIVATE_DIR=${PREFIX}/etc/conda/activate.d
+DEACTIVATE_DIR=${PREFIX}/etc/conda/deactivate.d
+mkdir -p "${ACTIVATE_DIR}"
+mkdir -p "${DEACTIVATE_DIR}"
+
+cp "${RECIPE_DIR}"/scripts/activate.sh "${ACTIVATE_DIR}"/sbcl-activate.sh
+cp "${RECIPE_DIR}"/scripts/deactivate.sh "${DEACTIVATE_DIR}"/sbcl-deactivate.sh
+
+chmod +x "${ACTIVATE_DIR}"/sbcl-activate.sh
+chmod +x "${DEACTIVATE_DIR}"/sbcl-deactivate.sh
