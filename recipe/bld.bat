fe379ca6277accb40fa25da2760c0f1b4b8a1cf1
memento <claude.rc@gmail.com>
2024-06-01 10:50:42 -0500

    (dbg) Do we still need extra def for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,13 @@
 @echo off
 
+:: Use existing conda sbcl as bootstrap
 call mamba install -y sbcl
 
+:: Build and install SBCL (builds in srouce dir and installs in PREFIX)
 cd %SRC_DIR%\sbcl-source
-  set "PATH=%BUILD_PREFIX%\Library\mingw-w64\bin;%PATH%"
-  set "CC=gcc"
-  set "CFLAGS=-I%BUILD_PREFIX%\Library\include %CFLAGS%"
+  :: set "PATH=%BUILD_PREFIX%\Library\mingw-w64\bin;%PATH%"
+  :: set "CC=gcc"
+  :: set "CFLAGS=-I%BUILD_PREFIX%\Library\include %CFLAGS%"
 
   bash make.sh --fancy > nul
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
@@ -16,9 +18,11 @@ cd %SRC_DIR%\sbcl-source
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cd %SRC_DIR%
 
+:: Copy the license files for conda-recipe compliance
 copy %SRC_DIR%\sbcl-source\COPYING %SRC_DIR%\COPYING > nul
 copy %SRC_DIR%\sbcl-source\CREDITS %SRC_DIR%\CREDITS > nul
 
+:: Setting conda host environment variables
 if not exist "%PREFIX%\etc\conda\activate.d\" mkdir "%PREFIX%\etc\conda\activate.d\"
 if not exist "%PREFIX%\etc\conda\deactivate.d\" mkdir "%PREFIX%\etc\conda\deactivate.d\"
 
