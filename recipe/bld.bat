ae75ca5cf18317863ce726b1d7e6727ffb99aa8a
memento <claude.rc@gmail.com>
2024-06-03 13:52:42 -0500

    (ref) update with aarch64 PR

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,9 @@
 @echo off
 
+:: Use existing conda sbcl as bootstrap
 call mamba install -y sbcl
 
+:: Build and install SBCL (builds in srouce dir and installs in PREFIX)
 cd %SRC_DIR%\sbcl-source
   set "PATH=%BUILD_PREFIX%\Library\mingw-w64\bin;%PATH%"
   set "CC=gcc"
@@ -16,9 +18,11 @@ cd %SRC_DIR%\sbcl-source
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cd %SRC_DIR%
 
+:: Copy the license files for conda-recipe compliance
 copy %SRC_DIR%\sbcl-source\COPYING %SRC_DIR%\COPYING > nul
 copy %SRC_DIR%\sbcl-source\CREDITS %SRC_DIR%\CREDITS > nul
 
+:: Setting conda host environment variables
 if not exist "%PREFIX%\etc\conda\activate.d\" mkdir "%PREFIX%\etc\conda\activate.d\"
 if not exist "%PREFIX%\etc\conda\deactivate.d\" mkdir "%PREFIX%\etc\conda\deactivate.d\"
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-set -ex
+# --- Functions ---
 
 function build_install_stage() {
+  # Builds SBCL under a custom stage_dir by copying the source files from src_dir (keeps original src)
+  # Installs SBCL under install_dir
   local src_dir=$1
   local stage_dir=$2
   local install_dir=$3
 
+  # Remember the current directory
   local current_dir
   current_dir=$(pwd)
 
+  # Prepare the stage/build directory
   mkdir -p "${stage_dir}"
   cp -r "${src_dir}"/* "${stage_dir}"
 
+  # Configure SBCL build arguments, like host architecture, enable fancy features
   if [[ "${target_platform}" == "linux-aarch64" ]]; then
     SBCL_ARGS=(--fancy --arch=arm64)
   elif [[ "${target_platform}" == "osx-arm64" ]]; then
@@ -21,6 +26,7 @@ function build_install_stage() {
     SBCL_ARGS=(--fancy)
   fi
 
+  # Build and install SBCL
   cd "${stage_dir}"
     bash make.sh "${SBCL_ARGS[@]}" > _sbcl_build.log 2>&1
 
@@ -31,21 +37,30 @@ function build_install_stage() {
   cd "${current_dir}"
 }
 
+# --- Main ---
+
+set -ex
+
+# Select the conda architectures that build from source
 if [[ "${target_platform}" == "osx-64" ]] || \
    [[ "${target_platform}" == "linux-64" ]] || \
    [[ "${target_platform}" == "linux-aarch64" ]]
 then
-  # sbcl is installed in the host environment if x-compiling
+  # When not cross-compiling, the existing SBCL needs to be installed in the build environment
   if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "0" ]]; then
     mamba install -y sbcl
     export CROSSCOMPILING_EMULATOR=""
   fi
+  # When cross-compiling, the build SBCL is installed in the build environment as a dependency
 
-  build_install_stage "${SRC_DIR}/sbcl-source" "${SRC_DIR}/_conda_stage1-build" "${SRC_DIR}/_conda_stage1-install"
-  cp -r "${INSTALL_ROOT}"/* "${PREFIX}" > /dev/null 2>&1
+  build_install_stage "${SRC_DIR}/sbcl-source" "${SRC_DIR}/_conda_stage1-build" "${PREFIX}"
 
+  # Copy the license and credits for conda-recipe packaging
   cp "${SRC_DIR}"/sbcl-source/COPYING "${SRC_DIR}"
   cp "${SRC_DIR}"/sbcl-source/CREDITS "${SRC_DIR}"
+
+# No previous conda version: Need to bootstrap. Once a version is released
+# this special case will be merged to the above
 elif [[ "${target_platform}" == "linux-ppc64le" ]]; then
   # Install the bootstrap binary in a temporary location
   export INSTALL_ROOT=${SRC_DIR}/_conda_bootstrap-install
@@ -56,8 +71,11 @@ elif [[ "${target_platform}" == "linux-ppc64le" ]]; then
   build_install_stage "${SRC_DIR}/sbcl-source" "${SRC_DIR}/_conda_stage1-build" "${SRC_DIR}/_conda_stage1-install"
   cp -r "${INSTALL_ROOT}"/* "${PREFIX}" > /dev/null 2>&1
 
+  # Copy the license and credits for conda-recipe packaging
   cp "${SRC_DIR}"/sbcl-source/COPYING "${SRC_DIR}"
   cp "${SRC_DIR}"/sbcl-source/CREDITS "${SRC_DIR}"
+
+# All other architectures install the pre-built SBCL (downloaded in SRC_DIR
 else
   export INSTALL_ROOT=$PREFIX
   export SBCL_HOME=${INSTALL_ROOT}/lib/sbcl
