48906c8f52f550631c3ebd0f97e8a8fad91fd525
memento <claude.rc@gmail.com>
2024-06-07 18:30:55 -0500

    (fix) windows build may be flaky. testsuite fails

--- recipe/build.sh
+++ recipe/build.sh
@@ -71,14 +71,10 @@ else
   sh install.sh
 fi
 
-# Install SBCL in conda-forge environment
-ACTIVATE_DIR=${PREFIX}/etc/conda/activate.d
-DEACTIVATE_DIR=${PREFIX}/etc/conda/deactivate.d
-mkdir -p "${ACTIVATE_DIR}"
-mkdir -p "${DEACTIVATE_DIR}"
-
-cp "${RECIPE_DIR}"/scripts/activate.sh "${ACTIVATE_DIR}"/sbcl-activate.sh
-cp "${RECIPE_DIR}"/scripts/deactivate.sh "${DEACTIVATE_DIR}"/sbcl-deactivate.sh
-
-chmod +x "${ACTIVATE_DIR}"/sbcl-activate.sh
-chmod +x "${DEACTIVATE_DIR}"/sbcl-deactivate.sh
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}-${CHANGE}.sh"
+done
