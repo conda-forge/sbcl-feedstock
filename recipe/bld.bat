9b9c7afdb079443d91845b8e69030426b78968b6
Claire's Monster <claude.rc@gmail.com>
2024-06-19 09:38:02 -0500

    feat: build aarch64, ppc64le and linux64 from source (#14)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,19 @@
 @echo off
 
+:: Use existing conda sbcl as bootstrap
 call mamba install -y sbcl
 
-cd %SRC_DIR%\sbcl-source
-  set "PATH=%BUILD_PREFIX%\Library\mingw-w64\bin;%PATH%"
+:: Build and install SBCL (builds in _conda-build dir and installs in PREFIX)
+mkdir %SRC_DIR%\_conda-build
+cd %SRC_DIR%\_conda-build
+  xcopy /E %SRC_DIR%\sbcl-source\* . > nul
+
+  set "PATH=%BUILD_PREFIX%\Library\ucrt64\bin;%PATH%"
   set "CC=gcc"
-  set "CFLAGS=-I%BUILD_PREFIX%\Library\include %CFLAGS%"
+  set "CFLAGS=-I%BUILD_PREFIX%\Library\ucrt64\include %CFLAGS%"
+
+  set "_build_prefix=%BUILD_PREFIX:\=/%"
+  bash -c "find %_build_prefix% -name gcc.exe"
 
   bash make.sh --fancy > nul
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
@@ -14,11 +22,20 @@ cd %SRC_DIR%\sbcl-source
   set "SBCL_HOME=%INSTALL_ROOT%/lib/sbcl"
   bash install.sh > nul
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+  :: Install dynamic library: There is an odd error with make trying to use '-lzstd' as a target
+  :: bash make-shared-library.sh > nul
+  :: if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+  :: copy src\runtime\libsbcl.dll %PREFIX%\bin\libsbcl.dll > nul
+  :: copy src\runtime\libsbcl.lib %PREFIX%\lib\libsbcl.lib > nul
+
 cd %SRC_DIR%
 
+:: Copy the license files for conda-recipe compliance
 copy %SRC_DIR%\sbcl-source\COPYING %SRC_DIR%\COPYING > nul
 copy %SRC_DIR%\sbcl-source\CREDITS %SRC_DIR%\CREDITS > nul
 
+:: Setting conda host environment variables
 if not exist "%PREFIX%\etc\conda\activate.d\" mkdir "%PREFIX%\etc\conda\activate.d\"
 if not exist "%PREFIX%\etc\conda\deactivate.d\" mkdir "%PREFIX%\etc\conda\deactivate.d\"
 
